6796855a1e79e22923062b52af8d0e3e8bb34db2
MindCatalyse <42488037+mindcatalyse@users.noreply.github.com>
2020-05-31 22:43:36 +0200

    Update createtableechellevraisemblance.sql

--- createtableechellevraisemblance.sql
+++ createtableechellevraisemblance.sql
@@ -1,4 +1,4 @@
-create table echellevraisemblance ( idvraisemblance integer primary key , vraisemblance varchar(50) );
+create table echellevraisemblance ( idvraisemblance integer primary key , vraisemblance varchar(150) );
 insert into echellevraisemblance ( idvraisemblance , vraisemblance )   values (  '1' , 'Faible  Peu probable ' );
 insert into echellevraisemblance ( idvraisemblance , vraisemblance )   values (  '2' , 'Moyenne  Possible ' );
 insert into echellevraisemblance ( idvraisemblance , vraisemblance )   values (  '3' , 'Elevee  Probable ' );
