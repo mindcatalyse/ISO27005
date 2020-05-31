85ccce8562251b01b02a8f6f3e28f44ff1c6ea5b
MindCatalyse <42488037+mindcatalyse@users.noreply.github.com>
2020-05-31 22:39:46 +0200

    Update createtablecritereacceptationrisque.sql

--- createtablecritereacceptationrisque.sql
+++ createtablecritereacceptationrisque.sql
@@ -1,4 +1,4 @@
-create table critereacceptationrisque ( idrisque integer primary key , risque varchar(50) , niveaurisquede varchar(50) , niveaurisquea varchar(50) , acceptationrisque varchar(50) );
+create table critereacceptationrisque ( idrisque integer primary key , risque varchar(150) , niveaurisquede varchar(150) , niveaurisquea varchar(150) , acceptationrisque varchar(150) );
 insert into critereacceptationrisque ( idrisque , risque , niveaurisquede , niveaurisquea , acceptationrisque )   values (  '1' , 'Faible' , '1' , '8' , 'Oui' );
 insert into critereacceptationrisque ( idrisque , risque , niveaurisquede , niveaurisquea , acceptationrisque )   values (  '2' , 'Moyen' , '9' , '23' , 'Non' );
 insert into critereacceptationrisque ( idrisque , risque , niveaurisquede , niveaurisquea , acceptationrisque )   values (  '3' , 'Eleve' , '24' , '48' , 'Non' );
